4091a366ab1fc44621ce6b44729841c34853ec8f
Gabriel Gomes <gbgta29@gmail.com>
2025-10-27 00:24:52 +0000

    troque o pixel do facebook para esse : 2211153889376706

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Script from 'next/script';
 import { Toaster } from "@/components/ui/toaster"
 import './globals.css';
 
-const PIXEL_ID = "2249818838798483";
+const PIXEL_ID = "2211153889376706";
 
 export const metadata: Metadata = {
   title: 'Conversa Digital',
